eaa9f9ccfd8bda6b958cb0356139914096303776
ImCarlos17 <carlosmba96@gmail.com>
2022-09-30 11:24:41 -0400

    delete Y letter in code

--- src/component/RecordsTable.js
+++ src/component/RecordsTable.js
@@ -12,7 +12,7 @@ const RecordsTable = () => {
       </div>
 
       <div className="flex flex-col items-center">
-        <h1 className="text-2xl"> BEST SCORES</h1>y
+        <h1 className="text-2xl"> BEST SCORES</h1>
         <table class="rounded-t-lg m-5 w-96 mx-auto bg-gray-200 text-gray-800 text-lg">
           <tr class="text-left border-b-2 border-gray-300">
             <th class="px-4 py-3">#</th>
